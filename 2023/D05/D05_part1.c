a39073e9518921592ec2ff79decd2cd8c148a1ab
jrummuka <jrummuka@student.hive.fi>
2024-02-19 18:30:16 +0200

    mishap with ints and longs took 4 hours to make ugly code :)

--- 2023/D05/D05_part1.c
+++ 2023/D05/D05_part1.c
@@ -6,12 +6,12 @@
 
 typedef struct brick
 {
-    int destination;
-    int start;
-    int range;
+    long destination;
+    long start;
+    long range;
 } brick;
 
-int seeds[100];
+long seeds[100];
 int slot;
 brick *seedtosoil[100];
 brick *soiltofert[100];
@@ -25,14 +25,93 @@ int solve()
 {
     int i = 0;
     int x = 0;
-    int value = 0;
+    long res = INT64_MAX;
+    long value = 0;
     while(seeds[i])
     {
         value = seeds[i];
+        printf("value of seed[%d]: %ld\n", i, value);
         while (seedtosoil[x])
         {
-            if (value )
+            if ((value > seedtosoil[x]->start) && (value < (seedtosoil[x]->start + seedtosoil[x]->range)))
+            {
+//                printf("seedtosoil[%d]: %d\ndest: %d, start: %d, range: %d\n", x, seeds[i], seedtosoil[x]->destination, seedtosoil[x]->start, seedtosoil[x]->range);
+                value += seedtosoil[x]->destination - seedtosoil[x]->start;
+                break;
+            }
+            x++;
         }
+        printf("value of soil: %ld\n", value);
+        x = 0;
+        while (soiltofert[x])
+        {
+            if ((value >= soiltofert[x]->start) && (value <= (soiltofert[x]->start + soiltofert[x]->range)))
+            {
+                value += soiltofert[x]->destination - soiltofert[x]->start;
+                break;
+            }
+            x++;
+        }
+        printf("value of fert: %ld\n", value);
+        x = 0;
+        while (ferttowater[x])
+        {
+            if ((value >= ferttowater[x]->start) && (value <= (ferttowater[x]->start + ferttowater[x]->range)))
+            {
+                value += ferttowater[x]->destination - ferttowater[x]->start;
+                break;
+            }
+            x++;
+        }
+        printf("value of water: %ld\n", value);
+        x = 0;
+        while (watertolight[x])
+        {
+            if ((value >= watertolight[x]->start) && (value <= (watertolight[x]->start + watertolight[x]->range)))
+            {
+                value += watertolight[x]->destination - watertolight[x]->start;
+                break;
+            }
+            x++;
+        }
+        printf("value of light: %ld\n", value);
+        x = 0;
+        while (lighttotemp[x])
+        {
+            if ((value >= lighttotemp[x]->start) && (value <= (lighttotemp[x]->start + lighttotemp[x]->range)))
+            {
+                value += lighttotemp[x]->destination - lighttotemp[x]->start;
+                break;
+            }
+            x++;
+        }
+        printf("value of temp: %ld\n", value);
+        x = 0;
+        while (temptohumid[x])
+        {
+            if ((value >= temptohumid[x]->start) && (value <= (temptohumid[x]->start + temptohumid[x]->range)))
+            {
+                value += temptohumid[x]->destination - temptohumid[x]->start;
+                break;
+            }
+            x++;
+        }
+        printf("value of humid: %ld\n", value);
+        x = 0;
+        while (humidtoloc[x])
+        {
+            if ((value >= humidtoloc[x]->start) && (value <= (humidtoloc[x]->start + humidtoloc[x]->range)))
+            {
+                value += humidtoloc[x]->destination - humidtoloc[x]->start;
+                break;
+            }
+            x++;
+        }
+        printf("value of loc: %ld\n\n", value);
+        x = 0;
+        if (res > value)
+            res = value;
+        i++;
     }
     return res;
 }
@@ -49,8 +128,8 @@ void saveseeds(char *str)
         {
             if (isdigit(token2[1]))
             {
-                seeds[i] = atoi(token2);
-                printf("seed added %d: %d\n", i, seeds[i]);
+                seeds[i] = atol(token2);
+                printf("seed added %d: %ld\n", i, seeds[i]);
                 i++;
             }
         }
@@ -64,11 +143,11 @@ void add_brick(char *str, int phase)
     char *token;
 
     token = strtok_r(str, " ", &str);
-        new->destination = atoi(token);
+        new->destination = atol(token);
     token = strtok_r(str, " ", &str);
-        new->start = atoi(token);
+        new->start = atol(token);
     token = strtok_r(str, " ", &str);
-        new->range = atoi(token);
+        new->range = atol(token);
     if (phase == 2)
         seedtosoil[slot] = new;
     else if (phase == 3)
@@ -83,7 +162,7 @@ void add_brick(char *str, int phase)
         temptohumid[slot] = new;
     else if (phase == 8)
         humidtoloc[slot] = new;
-    printf("New brick added to slot: %d - phase: %d\ndest: %d, start: %d, range %d\n", slot, phase, new->destination, new->start, new->range);
+    printf("New brick added to slot: %d - phase: %d\ndest: %ld, start: %ld, range %ld\n", slot, phase, new->destination, new->start, new->range);
 
 }
 
