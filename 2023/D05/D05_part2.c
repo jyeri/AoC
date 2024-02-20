323f7e3aa38bf9854c9df71797448aabce346a5a
jrummuka <jrummuka@student.hive.fi>
2024-02-20 14:53:35 +0200

    Brute force approach

--- 2023/D05/D05_part2.c
+++ 2023/D05/D05_part2.c
@@ -9,20 +9,22 @@ typedef struct brick
     long destination;
     long start;
     long range;
+    long end;
+    long offset;
 } brick;
 
-long deeds[100];
-long seeds[100];
-long seedse[100];
-long seedtosoil_max;
-long soiltofert_max;
-long ferttowater_max;
-long watertolight_max;
-long lighttotemp_max;
-long temptohumid_max;
-long humidtoloc_max;
-long maxilimian;
+typedef struct seeds
+{
+    long start;
+    long len;
+    long end;
+} seeds;
+
 int slot;
+long deeds[100];
+long rangemax;
+long rangemin;
+seeds *seedarr[100];
 brick *seedtosoil[100];
 brick *soiltofert[100];
 brick *ferttowater[100];
@@ -31,151 +33,157 @@ brick *lighttotemp[100];
 brick *temptohumid[100];
 brick *humidtoloc[100];
 
-int solve()
+long    phases(long seed)
+{
+    int x = 0;
+    while (seedtosoil[x])
+    {
+        if ((seed >= seedtosoil[x]->start) && (seed <= seedtosoil[x]->end))
+        {
+            seed += seedtosoil[x]->offset;
+            break;
+        }
+        x++;
+    }
+//    printf("value of soil: %ld\n", seed);
+    x = 0;
+    while (soiltofert[x])
+    {
+        if ((seed >= soiltofert[x]->start) && (seed <= soiltofert[x]->end))
+        {
+            seed += soiltofert[x]->offset;
+            break;
+        }
+        x++;
+    }
+//    printf("value of fert: %ld\n", seed);
+    x = 0;
+    while (ferttowater[x])
+    {
+        if ((seed >= ferttowater[x]->start) && (seed <= ferttowater[x]->end))
+        {
+            seed += ferttowater[x]->offset;
+            break;
+        }
+        x++;
+    }
+//    printf("value of water: %ld\n", seed);
+    x = 0;
+    while (watertolight[x])
+    {
+        if ((seed >= watertolight[x]->start) && (seed <= watertolight[x]->end))
+        {
+            seed += watertolight[x]->offset;
+            break;
+        }
+        x++;
+    }
+//    printf("value of light: %ld\n", seed);
+    x = 0;
+    while (lighttotemp[x])
+    {
+        if ((seed >= lighttotemp[x]->start) && (seed <= lighttotemp[x]->end))
+        {
+            seed += lighttotemp[x]->offset;
+            break;
+        }
+        x++;
+    }
+//    printf("value of temp: %ld\n", seed);
+    x = 0;
+    while (temptohumid[x])
+    {
+        if ((seed >= temptohumid[x]->start) && (seed <= temptohumid[x]->end))
+        {
+            seed += temptohumid[x]->offset;
+            break;
+        }
+        x++;
+    }
+//    printf("value of humid: %ld\n", seed);
+    x = 0;
+    while (humidtoloc[x])
+    {
+        if ((seed >= humidtoloc[x]->start) && (seed <= humidtoloc[x]->end))
+        {
+            seed += humidtoloc[x]->offset;
+            break;
+        }
+        x++;
+    }
+    return seed;
+}
+
+long solve()
 {
     int i = 0;
-    int x = 0;
-    int k = 0;
-    int amount;
     long res = INT64_MAX;
-    long value = 0;
-    while(seeds[i])
+    long s = 0;
+    long e = 0;
+    long hold = 0;
+    printf("in solve\n");
+    while(seedarr[i])
     {
-        amount = seedse[i];
-        k = 0;
-        while(amount--)
-        {
-            value = seeds[i] + k;
-            if (value > maxilimian)
-                break;
-            else
-            {
-                if (value < seedtosoil_max)
-                //printf("value of seed[%d]: %ld\n", i, value);
-                {
-                    while (seedtosoil[x])
-                    {
-                        if ((value > seedtosoil[x]->start) && (value < (seedtosoil[x]->start + seedtosoil[x]->range)))
-                        {
-//                            //printf("seedtosoil[%d]: %d\ndest: %d, start: %d, range: %d\n", x, seeds[i], seedtosoil[x]->destination, seedtosoil[x]->start, seedtosoil[x]->range);
-                            value += seedtosoil[x]->destination - seedtosoil[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                }
-                //printf("value of soil: %ld\n", value);
-                x = 0;
-                if (value < soiltofert_max)
-                {
-                    while (soiltofert[x])
-                    {
-                        if ((value >= soiltofert[x]->start) && (value <= (soiltofert[x]->start + soiltofert[x]->range)))
-                        {
-                            value += soiltofert[x]->destination - soiltofert[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                    //printf("value of fert: %ld\n", value);
-                }
-                x = 0;
-                if (value < ferttowater_max)
-                {
-                    while (ferttowater[x])
-                    {
-                        if ((value >= ferttowater[x]->start) && (value <= (ferttowater[x]->start + ferttowater[x]->range)))
-                        {
-                            value += ferttowater[x]->destination - ferttowater[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                    //printf("value of water: %ld\n", value);
-                }
-                x = 0;
-                if (value < watertolight_max)
-                {
-                    while (watertolight[x])
-                    {
-                        if ((value >= watertolight[x]->start) && (value <= (watertolight[x]->start + watertolight[x]->range)))
-                        {
-                            value += watertolight[x]->destination - watertolight[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                    //printf("value of light: %ld\n", value);
-                }
-                x = 0;
-                if (value < lighttotemp_max)
-                {
-                    while (lighttotemp[x])
-                    {
-                        if ((value >= lighttotemp[x]->start) && (value <= (lighttotemp[x]->start + lighttotemp[x]->range)))
-                        {
-                            value += lighttotemp[x]->destination - lighttotemp[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                    //printf("value of temp: %ld\n", value);
-                }
-                x = 0;
-                if (value < temptohumid_max)
-                {
-                    while (temptohumid[x])
-                    {
-                        if ((value >= temptohumid[x]->start) && (value <= (temptohumid[x]->start + temptohumid[x]->range)))
-                        {
-                            value += temptohumid[x]->destination - temptohumid[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                    //printf("value of humid: %ld\n", value);
-                }
-                x = 0;
-                if (value < humidtoloc_max)
-                {
-                    while (humidtoloc[x])
-                    {
-                        if ((value >= humidtoloc[x]->start) && (value <= (humidtoloc[x]->start + humidtoloc[x]->range)))
-                        {
-                            value += humidtoloc[x]->destination - humidtoloc[x]->start;
-                            break;
-                        }
-                        x++;
-                    }
-                    //printf("value of loc: %ld\n\n", value);
-                }
-                x = 0;
-            }
-            if (res > value)
-                res = value;
-            k++;
-        }
+        printf("Saved seeds on array:\n");
+        printf("seed[%d]\nstart: %ld - end: %ld\n", i, seedarr[i]->start, seedarr[i]->end);
         i++;
     }
-    return res;
+    i = 0;
+    // we would need to apply for whole range of the seeds instead at the time..
+    while (seedarr[i])
+    {
+        s = seedarr[i]->start;
+        e = seedarr[i]->end;
+        printf("iterating seeds: %ld -> %ld\n", s, e);
+        while (s <= e)
+        {
+            if (s >= rangemin && s <= rangemax)
+                hold = phases(s);
+            if (hold < res)
+                res = hold;
+            s++;
+        }
+        printf("SEEDSET %d DONE\n", i);
+        i++;
+    }
+    return (res);
+}
+
+seeds *createseed(long start, long len)
+{
+    printf("in createseed\n");
+    seeds *new;
+
+    new = malloc(sizeof(seeds));
+    new->len = len;
+    new->start = start;
+    new->end = len + start;
+    return new;
 }
 
 void part2seeds(int i)
 {
-    int x = 0;
-    long amount = 0;
+    printf("in part2seed\n");
     int j = 0;
+    int x = 0;
+    long start = 0;
+    long len = 0;
+    seeds seed;
+
     while (x < i)
     {
-        seeds[j] = deeds[x];
-        seedse[j] = deeds[x + 1];
-        x += 2;
+        start = deeds[x];
+        len = deeds[x + 1];
+        seedarr[j] = createseed(start, len);
+        printf("seed[%d] created\n", j);
         j++;
+        x += 2;
     }
 }
 
 void saveseeds(char *str)
 {
+    printf("in saveseeds\n");
     char    *token;
     char    *token2;
     int     i = 0;
@@ -208,51 +216,42 @@ void add_brick(char *str, int phase)
         new->start = atol(token);
     token = strtok_r(str, " ", &str);
         new->range = atol(token);
+    new->end = new->start + new->range;
+    new->offset = new->destination - new->start;
+    if (new->start < rangemin)
+        rangemin = new->start;
+    if (new->end > rangemax)
+        rangemax = new->end;
     if (phase == 2)
     {
         seedtosoil[slot] = new;
-        if (seedtosoil_max < new->start + new->range)
-            seedtosoil_max = new->start + new->range;
     }
     else if (phase == 3)
     {
         soiltofert[slot] = new;
-        if (soiltofert_max < new->start + new->range)
-            soiltofert_max = new->start + new->range;
     }
     else if (phase == 4)
     {
         ferttowater[slot] = new;
-        if (ferttowater_max < new->start + new->range)
-            ferttowater_max = new->start + new->range;
     }
     else if (phase == 5)
     {
         watertolight[slot] = new;
-        if (ferttowater_max < new->start + new->range)
-            ferttowater_max = new->start + new->range;
     }
     else if (phase == 6)
     {
         lighttotemp[slot] = new;
-        if (watertolight_max < new->start + new->range)
-            watertolight_max = new->start + new->range;
     }
     else if (phase == 7)
     {
         temptohumid[slot] = new;
-        if (temptohumid_max < new->start + new->range)
-            temptohumid_max = new->start + new->range;
     }
     else if (phase == 8)
     {
         humidtoloc[slot] = new;
-        if (humidtoloc_max < new->start + new->range)
-            humidtoloc_max = new->start + new->range;
     }
-    if (new->start + new->range > maxilimian)
-        maxilimian = new->start + new->range;
-//    //printf("New brick added to slot: %d - phase: %d\ndest: %ld, start: %ld, range %ld\n", slot, phase, new->destination, new->start, new->range);
+    printf("New brick added to slot: %d - phase: %d\ndest: %ld, start: %ld, range %ld\n", slot, phase, new->destination, new->start, new->range);
+    printf("range ends: %ld - offset: %ld\n\n", new->end, new->offset);
 
 }
 
@@ -290,10 +289,12 @@ int main(int argc, char **argv)
 {
     char    line[1024];
     FILE    *fptr;
-    int     result = 0;
+    long     result = 0;
     int     current = 0;
     int     phase = 0;
-    maxilimian = 0;
+
+    rangemax = INT64_MIN;
+    rangemin = INT64_MAX;
 
     if (argc < 2)
     {
@@ -313,10 +314,8 @@ int main(int argc, char **argv)
         phase = parser(line, phase);
     }
     fclose(fptr);
-    
     result = solve();
-    printf("result: %d\n", result);
+    printf("result: %ld\n", result);
     return result;
  
 }
-
