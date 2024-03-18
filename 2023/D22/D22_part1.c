a2c7f5f3ec7dda64b206fd4784cfa8340d2c8a7b
jrummuka <jrummuka@student.hive.fi>
2024-03-18 16:07:26 +0200

    D22_part1 DONE

--- 2023/D22/D22_part1.c
+++ 2023/D22/D22_part1.c
@@ -32,8 +32,15 @@ Brick   *create_brick()
     new->ex = -1;
     new->ey = -1;
     new->ez = -1;
+    int i = 0;
     new->give_support = (int *)malloc(sizeof(int) * 10);
     new->get_support = (int *)malloc(sizeof(int) * 10);
+    while(i < 10)
+    {
+        new->give_support[i] = -1;
+        new->get_support[i] = -1;
+        i++;
+    }
 
     return (new);
 }
@@ -82,7 +89,9 @@ void    sort_bricks(int max)
 bool    overlap(Brick *b1, Brick *b2)
 {
     if (fmax(b1->sx, b2->sx) <= fmin(b1->ex, b2->ex) && fmax(b1->sy, b2->sy) <= fmin(b1->ey, b2->ey))
+    {
         return true;
+    }
     return false;
 }
 
@@ -98,11 +107,13 @@ void    mark_falling(int maximum)
     int i = 0;
     int j = 0;
     int min_z = 1;
+//    int x = 0;
     Brick *b;
     Brick *bcomp;
 
     while (i < maximum)
     {
+        min_z = 1;
         b = brickarray[i];
         j = 1;
         while (i - j >= 0)
@@ -111,20 +122,98 @@ void    mark_falling(int maximum)
             if (overlap(b, bcomp))
             {
                 min_z = fmax(min_z, bcomp->ez + 1);
+                // Move this, bit slower but much more reasonable, and easier to manage that only 1 belove can support
+//                b->get_support[x] = i - j;
+//                k = 0;
+//                while(bcomp->give_support[k] > 0)
+//                    k++;
+//                bcomp->give_support[k] = i;
+//                printf("brickarray[%c] is supported by brickarray[%c]\n", 'a' + i, 'a' + (i - j));
+//                x++;
             }
             j++;
         }
-        b->sz = min_z - b->ez;
-        b->ez = min_z;
+        b->ez -= b->sz - min_z;
+        b->sz = min_z;
         i++;
     }
 
 }
 
-//void    mark_supports(int max)
-//{
-//
-//}
+void    mark_supports(int maximum)
+{
+    int i = maximum - 1;
+    int j = maximum - 1;
+    int x = 0;
+    int k = 0;
+    Brick *lower;
+    Brick *upper;
+    lower = NULL;
+    upper = NULL;
+
+    while (i >= 0)
+    {
+        upper = brickarray[i];
+        j = i - 1;
+        x = 0;
+        k = 0;
+        while (j >= 0)
+        {
+            lower = brickarray[j];
+            if (overlap(lower, upper) && (upper->sz == lower->ez + 1))
+            {
+                upper->get_support[x] = j;
+                k = 0;
+                while (lower->give_support[k] > -1)
+                {
+                    k++;
+                }
+                lower->give_support[k] = i;
+                printf("brickarray[%c]->[%d] supports brickarray[%c]\n", 'a' + j, k, 'a' + i);
+                x++;
+            }
+            j--;
+        }
+        i--;
+    }
+}
+
+void    solve(int maximum)
+{
+    int i = 0;
+    int x = 0;
+    int k = 0;
+    int sup = 0;
+    int total = 0;
+    Brick *brick;
+    Brick *supported;
+
+    while (i < maximum)
+    {
+        brick = brickarray[i];
+        while (brick->give_support[x] > -1)
+        {
+            supported = brickarray[brick->give_support[x]];
+            k = 0;
+            while (supported->get_support[k] > -1)
+                k++;
+            if (k < 2)
+                sup--;
+            else
+                sup++;
+            x++;
+        }
+        if(sup == x)
+        {
+            printf("Brick %c CAN disengrate\n", 'a' + i);
+            total += 1;
+        }
+        sup = 0;
+        x = 0;
+        i++;
+    }
+    printf("totalling: %d\n", total);
+}
 
 int     parser(char *line, int current)
 {
@@ -196,8 +285,8 @@ int     main(int argc, char **argv)
     {
          printf("Brick %d: [%d, %d, %d] ~ [%d, %d, %d]\n", i,  brickarray[i]->sx, brickarray[i]->sy, brickarray[i]->sz, brickarray[i]->ex, brickarray[i]->ey, brickarray[i]->ez);
     }
-//    mark_supports(current);
-//    solve(current);
+    mark_supports(current);
+    solve(current);
     fclose(fptr);
     return current;
  
