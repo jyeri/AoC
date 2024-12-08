6f4e97931e46a5605387c13e52e144c57820b34e
jrummuka <jrummuka@student.hive.fi>
2024-12-08 10:48:33 +0200

    commenting out debug texts

--- 2024/Day8/JS/Day8.js
+++ 2024/Day8/JS/Day8.js
@@ -21,7 +21,7 @@ let maxRows = 0;
 let maxCols = 0;
 
 const addAntinodes = (x, y) => {
-    console.log("Adding antinode:", x, y);
+//    console.log("Adding antinode:", x, y);
     if (x >= 0 && x < maxRows && y >= 0 && y < maxCols) {
         antinodes.push([x, y]);
     }
@@ -83,7 +83,7 @@ const solvePart1 = () => {
     const uniqueAntinodes = new Set(antinodes.map(JSON.stringify));
     const finalAntinodes = Array.from(uniqueAntinodes).map(JSON.parse);
 
-    console.log("Unique Antinodes:", finalAntinodes);
+//    console.log("Unique Antinodes:", finalAntinodes);
     console.log("Part 1:", finalAntinodes.length);
 };
 
@@ -118,9 +118,9 @@ const solvePart2 = () => {
             if (antennas[i][0] !== antennas[j][0]) {
                 break;
             }
-            console.log("Antennas:");
-            console.log(antennas[i]);
-            console.log(antennas[j]);
+//            console.log("Antennas:");
+//            console.log(antennas[i]);
+//            console.log(antennas[j]);
 
             const x1 = antennas[i][1];
             const y1 = antennas[i][2];
@@ -131,7 +131,7 @@ const solvePart2 = () => {
 
             let antinode1 = [2 * x1 - x2, 2 * y1 - y2];
             let antinode2 = [2 * x2 - x1, 2 * y2 - y1];
-            console.log("UP:");
+//            console.log("UP:");
             addAntinodes(antennas[i][1], antennas[i][2]);
             addAntinodes(antennas[j][1], antennas[j][2]);
             while (antinode1[0] >= 0 && antinode1[0] < maxRows && antinode1[1] >= 0 && antinode1[1] < maxCols) {
@@ -140,7 +140,7 @@ const solvePart2 = () => {
                 antinode1[1] -= dy;
             }
     
-            console.log("DOWN:");
+//            console.log("DOWN:");
             while (antinode2[0] >= 0 && antinode2[0] < maxRows && antinode2[1] >= 0 && antinode2[1] < maxCols) {
                 addAntinodes(antinode2[0], antinode2[1]);
                 antinode2[0] += dx;
@@ -152,16 +152,16 @@ const solvePart2 = () => {
     const uniqueAntinodes = new Set(antinodes.map(JSON.stringify));
     const finalAntinodes = Array.from(uniqueAntinodes).map(JSON.parse);
 
-    let resgrid = new Array(maxRows).fill().map(() => new Array(maxCols).fill('.'));
-    for (const [x, y] of finalAntinodes) {
-        resgrid[x][y] = '#';
-    }
+//    let resgrid = new Array(maxRows).fill().map(() => new Array(maxCols).fill('.'));
+//    for (const [x, y] of finalAntinodes) {
+//        resgrid[x][y] = '#';
+//    }
 
-    console.log("Unique Antinodes:", finalAntinodes);
+//    console.log("Unique Antinodes:", finalAntinodes);
     console.log("Part 2:", finalAntinodes.length);
-    for(let i = 0; i < resgrid.length; i++) {
-        console.log(resgrid[i].join(''));
-    }
+//    for(let i = 0; i < resgrid.length; i++) {
+//        console.log(resgrid[i].join(''));
+//    }
 };
 
 solvePart1();
