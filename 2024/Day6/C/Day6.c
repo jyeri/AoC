2d3fea0047c5486c22bcc6ec9c3ac8aac6af7777
jrummuka <jrummuka@student.hive.fi>
2024-12-08 13:21:13 +0200

    D06 p1, C. finally found time to start C coding

--- 2024/Day6/C/Day6.c
+++ 2024/Day6/C/Day6.c
@@ -6,7 +6,9 @@
 typedef struct {
     int x;
     int y;
+    char dir;
 } Direction;
+
 
 char* read_file(const char* filename) {
     FILE* file = fopen(filename, "r");
@@ -61,24 +63,185 @@ char **read_input(const char *filename, int *out_row_count) {
     return array;
 }
 
-int solvePart1(char** input, int rows) {
-    int part1_res = 0;
+int inbounds(int x, int y, int rows, int cols) {
+    return x >= 0 && x < rows && y >= 0 && y < cols;
+}
 
-    for (int i = 0; i < rows; i++) {
-        printf("Line: %s\n", input[i]);
+int solvePart1(char** input_org, int rows) {
+    int part1_res = 0;
+    Direction start;
+    Direction current;
+    Direction next;
+    Direction UP = {-1, 0, 'U'};
+    Direction DOWN = {1, 0, 'D'};
+    Direction LEFT = {0, -1, 'L'};
+    Direction RIGHT = {0, 1, 'R'};
+    Direction dir[] = 
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT,
+    };
+
+    char *tester[] = {
+        "....#.....",
+        ".........#",
+        "..........",
+        "..#.......",
+        ".......#..",
+        "..........",
+        ".#..^.....",
+        "........#.",
+        "#.........",
+        "......#...",
+    };
+    int tester_rows = 10;
+    int tester_cols = strlen(tester[0]);
+    int cols = strlen(input_org[0]);
+
+    char **modifiable_tester = malloc(tester_rows * sizeof(char *));
+    for (int i = 0; i < tester_rows; i++) {
+        modifiable_tester[i] = strdup(tester[i]);
     }
 
+    char **input = malloc(rows * sizeof(char *));
+    for (int i = 0; i < rows; i++) {
+        input[i] = strdup(input_org[i]);
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (input[i][j] == '^') {
+                start.y = j;
+                start.x = i;
+                start.dir = 'U';
+                printf("Start: %d, %d\n", start.x, start.y);
+                break;
+            }
+        }
+    }
+
+    int dirInd = 0;
+    current = start;
+
+    while (inbounds(current.x, current.y, rows, cols)) {
+        printf("Current: %d, %d\n", current.x, current.y);
+        next.x = current.x + dir[dirInd].x;
+        next.y = current.y + dir[dirInd].y;
+        if (!inbounds(next.x, next.y, rows, cols)) {
+            current = next;
+            part1_res++;
+            break;
+        }
+        if (input[next.x][next.y] == '#') {
+            printf("Hit wall\n");
+            dirInd++;
+            if (dirInd == 4) {
+                dirInd = 0;
+            }
+            continue;
+        }
+        current = next;
+        if (input[current.x][current.y] == '.') {
+            input[current.x][current.y] = 'X'; 
+            part1_res++;
+        }
+    }
+
+    printf("exit at: %d, %d\n", current.x, current.y);
     printf("Part 1 result: %d\n", part1_res);
+
+    free(modifiable_tester);
+    free(input);
     return part1_res;
 }
 
-int solvePart2(char** input, int rows) {
+int solvePart2(char** input_org, int rows) {
     int part2_res = 0;
 
-    for (int i = 0; i < rows; i++) {
-        printf("Line: %s\n", input[i]);
+    Direction start;
+    Direction current;
+    Direction next;
+    Direction UP = {-1, 0, 'U'};
+    Direction DOWN = {1, 0, 'D'};
+    Direction LEFT = {0, -1, 'L'};
+    Direction RIGHT = {0, 1, 'R'};
+    Direction dir[] = 
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT,
+    };
+
+    char *tester[] = {
+        "....#.....",
+        ".........#",
+        "..........",
+        "..#.......",
+        ".......#..",
+        "..........",
+        ".#..^.....",
+        "........#.",
+        "#.........",
+        "......#...",
+    };
+    int tester_rows = 10;
+    int tester_cols = strlen(tester[0]);
+    int cols = strlen(input_org[0]);
+
+    char **modifiable_tester = malloc(tester_rows * sizeof(char *));
+    for (int i = 0; i < tester_rows; i++) {
+        modifiable_tester[i] = strdup(tester[i]);
     }
 
+    char **input = malloc(rows * sizeof(char *));
+    for (int i = 0; i < rows; i++) {
+        input[i] = strdup(input_org[i]);
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (input[i][j] == '^') {
+                start.y = j;
+                start.x = i;
+                start.dir = 'U';
+                printf("Start: %d, %d\n", start.x, start.y);
+            }
+        }
+    }
+
+    int dirInd = 0;
+    current = start;
+
+    while (inbounds(current.x, current.y, rows, cols)) {
+        printf("Current: %d, %d\n", current.x, current.y);
+        next.x = current.x + dir[dirInd].x;
+        next.y = current.y + dir[dirInd].y;
+        if (!inbounds(next.x, next.y, rows, cols)) {
+            current = next;
+            part2_res++;
+            break;
+        }
+        if (input[next.x][next.y] == '#') {
+            printf("Hit wall\n");
+            dirInd++;
+            if (dirInd == 4) {
+                current.dir = dir[dirInd].dir;
+                dirInd = 0;
+            }
+            continue;
+        }
+        current = next;
+        if (input[current.x][current.y] == '.') {
+            input[current.x][current.y] = current.dir;
+            part2_res++;
+        }
+    }
+    
+    free(modifiable_tester);
+    free(input);
     printf("Part 2 Result: %d\n", part2_res);
     return part2_res;
 }
